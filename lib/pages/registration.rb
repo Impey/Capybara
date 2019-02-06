45c135039349d5f52f218e23a1bbf56c9d9b400d
Tech-A42 <tech-a42@tech-a42s-air.home>
2019-02-06 19:17:47 +0000

    Tested every field on the page

--- lib/pages/registration.rb
+++ lib/pages/registration.rb
@@ -2,17 +2,30 @@ require 'capybara/dsl'
 
 class Registration
   include Capybara::DSL
-  num = rand(5..6)
   #page object
   REGISTRATION_PAGE_URL = 'http://localhost:9292/'
   FIRST_NAME_FIELD_ID = 'firstName'
   LAST_NAME_FIELD_ID = 'lastName'
   AGE_FIELD = '/html/body/div/form/div[3]/div/input'
   DOB_FIELD = 'dob'
+  num = rand(5..6)
   GENDER_BTN = "/html/body/div/form/div[#{num}]"
-  DEGREE_FIELD = "/html/body/div/form/div[7]/div/input"
+  DEGREE_FIELD = '/html/body/div/form/div[7]/div/input'
   UNIVERSITY_BOX = 'inputUni'
   UNI = 'University of Oxford'
+  ADDRESS_FIELD = 'inputAddress'
+  CITY_FIELD = 'inputCity'
+  INPUT_COUNTY = 'inputCounty'
+  COUNTY = 'Hertfordshire'
+  POSTCODE_FIELD = 'inputPostcode'
+  EMAIL_FIELD = 'inputemailaddress'
+  SKILLS_FIELD = 'exampleFormControlTextarea1'
+  PHONE_FIELD ='exampleFormControlInput1'
+  LINKEDIN_FIELD ='/html/body/div/form/div[17]/div/input'
+  STREAM_BTN = '/html/body/div/form/div[20]/div[1]/label'
+  TANDC = 'terms'
+  SLIDER = 'experienceSlider'
+  SIGNIN_BTN = '/html/body/div/form/button'
 
  
 
@@ -47,6 +60,56 @@ class Registration
   def select_university
     select(UNI, :from => UNIVERSITY_BOX)
   end
+
+  def fill_in_address_field(add)
+    fill_in(ADDRESS_FIELD, :with => add)
+  end
+
+  def fill_in_city_field(city)
+    fill_in(CITY_FIELD, :with => city)
+  end
+
+  def select_county
+    select(COUNTY, :from => INPUT_COUNTY)
+  end
+
+  def fill_in_postcode_field(postcode)
+    fill_in(POSTCODE_FIELD, :with => postcode)
+  end
+
+  def fill_in_email_field(email)
+    fill_in(EMAIL_FIELD, :with => email)
+  end
+  
+  def fill_in_skills_field(skills)
+    fill_in(SKILLS_FIELD, :with => skills)
+  end
+
+  def fill_in_phone_field(phone)
+    fill_in(PHONE_FIELD, :with => phone)
+  end
+
+  def fill_linkedin_field(link)
+    find(:xpath,LINKEDIN_FIELD).set(link)
+  end
+
+  def check_gender_btn
+    find(:xpath,STREAM_BTN).click
+  end
+
+  def check_tandc_btn
+    check(TANDC)
+  end
+
+  def set_slider
+    find(SLIDER).value(99)
+  end
+
+  def click_signin_btn
+    find(:xpath,SIGNIN_BTN).click
+  end
+
+
 
 
  
--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -13,6 +13,18 @@ describe 'testing a correct path for sparta registration' do
    @sparta_demo_site.registration_page.check_gender_btn
    @sparta_demo_site.registration_page.fill_degree_field('Computing')
    @sparta_demo_site.registration_page.select_university
+   @sparta_demo_site.registration_page.fill_in_address_field('123 cool street')
+   @sparta_demo_site.registration_page.fill_in_city_field('New York')
+   @sparta_demo_site.registration_page.select_county
+   @sparta_demo_site.registration_page.fill_in_postcode_field('DD7 FJN')
+   @sparta_demo_site.registration_page.fill_in_email_field('Jeff@gamil.com')
+   @sparta_demo_site.registration_page.fill_in_skills_field('Saying my name jeff')
+   @sparta_demo_site.registration_page.fill_in_phone_field('01268792810')
+   @sparta_demo_site.registration_page.fill_linkedin_field('linkedin.com')
+   @sparta_demo_site.registration_page.check_gender_btn
+   @sparta_demo_site.registration_page.check_tandc_btn
+   @sparta_demo_site.registration_page.click_signin_btn
+   #@sparta_demo_site.registration_page.set_slider
    sleep 10
   end
 
