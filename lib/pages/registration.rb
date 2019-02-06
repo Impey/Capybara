3e93dc1a2d15988d10d2f399fd0756f04d17c0da
Tech-A42 <tech-a42@Tech-A42s-MacBook-Air.local>
2019-02-06 16:23:57 +0000

    added radio button test

--- lib/pages/registration.rb
+++ lib/pages/registration.rb
@@ -6,6 +6,12 @@ class Registration
   #page object
   REGISTRATION_PAGE_URL = 'http://localhost:9292/'
   FIRST_NAME_FIELD_ID = 'firstName'
+  LAST_NAME_FIELD_ID = 'lastName'
+  AGE_FIELD = '/html/body/div/form/div[3]/div/input'
+  DOB_FIELD = 'dob'
+  GENDER_BTN = '/html/body/div/form/div[6]/label/text()'
+
+ 
 
   def vist_registration_page
     visit(REGISTRATION_PAGE_URL)
@@ -14,6 +20,24 @@ class Registration
   def fill_in_name_field(name)
     fill_in(FIRST_NAME_FIELD_ID, :with => name)
   end
+
+  def fill_in_last_name_field(name)
+    fill_in(LAST_NAME_FIELD_ID, :with => name)
+  end
+
+  def fill_age_field(age)
+   find(:xpath,'/html/body/div/form/div[3]/div/input').set(age)
+  end
+
+  def fill_dob_field(dob)
+   fill_in(DOB_FIELD, :with => dob)
+  end
+
+  def check_gender_btn
+    num = rand(5..6)
+    find(:xpath,"/html/body/div/form/div[#{num}]/label").click
+  end
+
 
  
 
--- spec/registration_spec.rb
+++ spec/registration_spec.rb
@@ -7,7 +7,12 @@ describe 'testing a correct path for sparta registration' do
    @sparta_demo_site = SpartaDemoSite.new
    @sparta_demo_site.registration_page.vist_registration_page
    @sparta_demo_site.registration_page.fill_in_name_field('Tom')
-   sleep 5
+   @sparta_demo_site.registration_page.fill_in_last_name_field('Impey')
+   @sparta_demo_site.registration_page.fill_age_field(25)
+   @sparta_demo_site.registration_page.fill_dob_field("11/10/2019")
+   @sparta_demo_site.registration_page.check_gender_btn
+  
+   sleep 10
   end
 
 end
